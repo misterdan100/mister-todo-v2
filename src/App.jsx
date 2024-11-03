9bb2050bea06133f8eefdb1d838d6735d362b651
misterdan100 <danielmca60@gmail.com>
2024-11-03 14:33:05 -0500

    fix: router for undefined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import TasksProvider from './context/TasksProvider'
-import Layout from './layouts/Layout'
-import Home from './pages/Home'
-import Tasks from './pages/Tasks'
-import Projects from './pages/Projects'
-import Tags from './pages/Tags'
-import Favorites from './pages/Favorites'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import TasksProvider from "./context/TasksProvider";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import Projects from "./pages/Projects";
+import Tags from "./pages/Tags";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <TasksProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />}/>
-            <Route path='/tasks' element={<Tasks />}/>
-            <Route path='/projects' element={<Projects />}/>
-            <Route path='/tags' element={<Tags />}/>
-            <Route path='/favorites' element={<Favorites />}/>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/tags" element={<Tags />} />
+            <Route path="/favorites" element={<Favorites />} />
           </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </TasksProvider>
-    
-  )
+  );
 }
 
-export default App
+export default App;
